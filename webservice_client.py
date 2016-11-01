22e3024b944ccdf8d4a6e47ddbbc0215efd2db73
yunhu <cyy@yhucloud.com>
2016-11-01 17:54:14 +0800

    修改

--- webservice_client.py
+++ webservice_client.py
@@ -7,14 +7,19 @@ from suds.xsd.doctor import Import, ImportDoctor
 
 url = "http://localhost:8080/test?wsdl"
 
-# 引入schemas
+# 引入schemas，可不写
+# 在出现type ... 无法在http://schemas.xmlsoap.org/soap/encoding/找到等错误下需要引入
+
 imp = Import('http://xml.apache.org/xml-soap')
 imp2 = Import('http://schemas.xmlsoap.org/soap/encoding/')
-
 # 命名空间filter
 imp.filter.add('http://www.test/web/')
-client = Client(url, doctor=ImportDoctor(imp,imp2))
 
+# 一般情况下用这个就可以: 
+# client = Client(url) 
+
+client = Client(url, doctor=ImportDoctor(imp,imp2))
+print client
 result = client.service.get_test_method("test001")
 print result
 retMap = json.loads(result)
